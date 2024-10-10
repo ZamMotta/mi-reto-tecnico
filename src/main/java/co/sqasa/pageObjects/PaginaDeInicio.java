99f33e736350370c945743fb58e858278eeffe3c
ZamMotta <oezm@outlook.es>
2024-10-10 16:03:49 -0500

    Actualización

--- src/main/java/co/sqasa/pageObjects/PaginaDeInicio.java
+++ src/main/java/co/sqasa/pageObjects/PaginaDeInicio.java
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 package co.sqasa.pageObjects;
-=======
-package co.sqasa.pageObjects; 
->>>>>>> 3c488b1e7d9c12a4939a300f0ebbe63019263be6
 
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.By;
 
 public class PaginaDeInicio extends PageObject {
-    
+
     public void agregarProductoAlCarrito(int productoId, int cantidad) {
         find(By.id("producto-" + productoId)).click();
         find(By.id("cantidad")).clear();
@@ -17,10 +13,6 @@ public class PaginaDeInicio extends PageObject {
     }
 
     public void verificarProductosEnElCarrito() {
-<<<<<<< HEAD
-=======
         
-        
->>>>>>> 3c488b1e7d9c12a4939a300f0ebbe63019263be6
     }
 }
--- test/resources/features/tes.feature
+++
@@ -1,7 +0,0 @@
-Feature: Carrito de compras
-
-  Scenario: Agregar 2 productos al carrito de compras
-    Given el usuario está en la página de inicio
-    When el usuario agrega el primer producto con 2 cantidades al carrito
-    And el usuario agrega el segundo producto con 5 cantidades al carrito
-    Then el usuario debería ver ambos productos en el carrito con las cantidades correctas
